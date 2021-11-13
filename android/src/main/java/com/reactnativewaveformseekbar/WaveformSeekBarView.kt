3c488cd087326bfa1d50a0cecbe9cd920394dc05
Kuss <2360314753@qq.com>
2021-11-13 17:38:20 +0800

    :recycle: refactor: com.github.massoudss:waveformSeekBar to 4.1.0

--- android/src/main/java/com/reactnativewaveformseekbar/WaveformSeekBarView.kt
+++ android/src/main/java/com/reactnativewaveformseekbar/WaveformSeekBarView.kt
@@ -1,11 +1,7 @@
 package com.reactnativewaveformseekbar
 
 import android.content.Context
-import com.facebook.react.uimanager.UIManagerHelper
-import com.facebook.react.uimanager.UIManagerModule
-import com.masoudss.lib.SeekBarOnProgressChanged
 import com.masoudss.lib.WaveformSeekBar
-
 
 class WaveformSeekBarView(context: Context) : WaveformSeekBar(context) {
   override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
